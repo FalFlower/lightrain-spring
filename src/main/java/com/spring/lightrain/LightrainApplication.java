48c951ab8595f25af4334e1de76c773e3ce143d9
FalFlower <zt847269989@outlook.com>
2020-03-23 18:49:39 +0800

    commit 1.0

---
+++ src/main/java/com/spring/lightrain/LightrainApplication.java
@@ -0,0 +1,13 @@
+package com.spring.lightrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/lightrain/LightrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lightrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
